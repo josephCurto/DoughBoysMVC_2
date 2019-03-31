1394e5ebc4ecd15daa217bd8dbcfded32fa718f8
trinilime <44830296+trinilime@users.noreply.github.com>
2019-03-31 18:14:21 -0400

    first push

--- DoughBoysMVC/Controllers/HomeController.cs
+++ DoughBoysMVC/Controllers/HomeController.cs
@@ -4,40 +4,35 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using DoughBoysMVC.Models;
+//using DoughBoysMVC.Models;
 
 namespace DoughBoysMVC.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
+        public ActionResult Menu()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public ActionResult PreOrder()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }
+
+        public ActionResult Questions()
+        {
+            return View();
+        }
+
+        public ActionResult AboutUs()
+        {
+            return View();
         }
     }
 }
