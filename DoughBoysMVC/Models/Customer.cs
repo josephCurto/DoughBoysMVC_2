8c7f6bcc32c34b3a27a3840aa7ae5ba86dbb3606
Joseph Curto <44935373+josephCurto@users.noreply.github.com>
2019-04-07 12:48:47 -0400

    Adding error message

--- DoughBoysMVC/Models/Customer.cs
+++ DoughBoysMVC/Models/Customer.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace DoughBoysMVC.Models
 {
     public class Customer
     {
-        public string PhoneNo { get; set; }
+       [Required(ErrorMessage = "Please enter your first name")]
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string PhoneNo { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+", 
+        ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
     }
 }
